448ce5f54a5aa9e77263f218c5e3281e05365f7a
bstorozhuk <storozhuk.b.m@gmail.com>
2016-12-02 19:41:58 +0200

    Issue #12 aligning with master branch + Copyright info

--- src/jmh/java/io/github/robwin/ratelimiter/RateLimiterBenchmark.java
+++ src/jmh/java/io/github/robwin/ratelimiter/RateLimiterBenchmark.java
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright 2015 Robert Winkler and Bohdan Storozhuk
+ *  Copyright 2016 Robert Winkler and Bohdan Storozhuk
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
