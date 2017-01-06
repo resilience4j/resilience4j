af805130aab76ff2a1500ea973854631ab946307
Robert Winkler <rwinkler@telekom.de>
2017-01-06 14:23:08 +0100

    Released v0.8.0

--- src/jmh/java/io/github/robwin/circuitbreaker/internal/RingBitSetBenchmark.java
+++ src/jmh/java/io/github/robwin/circuitbreaker/internal/RingBitSetBenchmark.java
@@ -16,21 +16,9 @@
  *
  *
  */
-package io.github.robwin.circuitbreaker;
+package io.github.robwin.circuitbreaker.internal;
 
-import io.github.robwin.circuitbreaker.internal.RingBitSet;
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Fork;
-import org.openjdk.jmh.annotations.Group;
-import org.openjdk.jmh.annotations.GroupThreads;
-import org.openjdk.jmh.annotations.Measurement;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.Setup;
-import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.infra.Blackhole;
 
 import java.util.concurrent.TimeUnit;
