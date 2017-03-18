fbd7b799ebdf22993274c048f7d28092380f2bed
Chris Pilsworth <cpilsworth@gmail.com>
2017-03-18 07:56:13 +0000

    Move depdendency versions to gradle.properties so they can be loaded by integration test
    Rather than try and keep the versions in sync across the gradle build and test the version numbers are extracted to a properties file and loaded from there

--- src/test/java/io/github/robwin/osgi/OsgiIntegrationTest.java
+++ src/test/java/io/github/robwin/osgi/OsgiIntegrationTest.java
@@ -9,12 +9,14 @@ import org.ops4j.pax.exam.junit.PaxExam;
 import org.ops4j.pax.exam.options.UrlProvisionOption;
 import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerClass;
-import org.ops4j.pax.exam.util.PathUtils;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 
 import javax.inject.Inject;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 
 import static org.junit.Assert.assertEquals;
 import static org.ops4j.pax.exam.CoreOptions.bundle;
@@ -30,13 +32,15 @@ public class OsgiIntegrationTest {
     private BundleContext bundleContext;
 
     @Configuration
-    public Option[] config() {
+    public Option[] config() throws IOException {
+        Properties props = new Properties();
+        props.load(new FileReader("gradle.properties"));
         return options(
-                mavenBundle("io.javaslang", "javaslang", "2.0.5"),
-                mavenBundle("io.reactivex.rxjava2", "rxjava", "2.0.6"),
-                mavenBundle("org.slf4j",  "slf4j-api", "1.7.24"),
-                mavenBundle("javax.cache",  "cache-api", "1.0.0"),
-                mavenBundle("org.reactivestreams", "reactive-streams", "1.0.0"),
+                mavenBundle("io.javaslang", "javaslang", props.getProperty("javaslangVersion")),
+                mavenBundle("io.reactivex.rxjava2", "rxjava", props.getProperty("rxjavaVersion")),
+                mavenBundle("org.slf4j",  "slf4j-api", props.getProperty("slf4jApiVersion")),
+                mavenBundle("javax.cache",  "cache-api", props.getProperty("cacheApiVersion")),
+                mavenBundle("org.reactivestreams", "reactive-streams", props.getProperty("reactiveStreamsVersion")),
                 projectBundle(),
                 junitBundles());
     }
