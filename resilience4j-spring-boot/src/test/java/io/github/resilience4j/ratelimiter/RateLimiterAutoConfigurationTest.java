6471a50d50992f4242c6c9a61fffb57e8144ae9e
bstorozhuk <storozhuk.b.m@gmail.com>
2017-05-08 21:03:20 +0300

    test fix

--- resilience4j-spring-boot/src/test/java/io/github/resilience4j/ratelimiter/RateLimiterAutoConfigurationTest.java
+++ resilience4j-spring-boot/src/test/java/io/github/resilience4j/ratelimiter/RateLimiterAutoConfigurationTest.java
@@ -68,7 +68,7 @@ public class RateLimiterAutoConfigurationTest {
         RateLimiter rateLimiter = rateLimiterRegistry.rateLimiter(DummyService.BACKEND);
         assertThat(rateLimiter).isNotNull();
         await()
-            .atMost(1, TimeUnit.SECONDS)
+            .atMost(2, TimeUnit.SECONDS)
             .until(() -> rateLimiter.getMetrics().getAvailablePermissions() == 10);
 
         try {
@@ -109,7 +109,7 @@ public class RateLimiterAutoConfigurationTest {
         assertThat(lastEvent.getRateLimiterEventType()).isEqualTo(RateLimiterEvent.Type.FAILED_ACQUIRE);
 
         await()
-            .atMost(1, TimeUnit.SECONDS)
+            .atMost(2, TimeUnit.SECONDS)
             .until(() -> rateLimiter.getMetrics().getAvailablePermissions() == 10);
     }
 }
