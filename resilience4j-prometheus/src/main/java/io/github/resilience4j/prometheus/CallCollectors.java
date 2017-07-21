5e2ee1d466138fa02274449a4a3d529a4968f3da
Oleksandr Goldobin <o.goldobin@oberthur.com>
2017-07-21 12:10:46 +0200

    Issue #71: Fix issues found by Codacy

--- resilience4j-prometheus/src/main/java/io/github/resilience4j/prometheus/CallCollectors.java
+++ resilience4j-prometheus/src/main/java/io/github/resilience4j/prometheus/CallCollectors.java
@@ -4,9 +4,9 @@ import io.prometheus.client.Counter;
 import io.prometheus.client.Histogram;
 
 final class CallCollectors {
-    final Histogram histogram;
-    final Counter totalCounter;
-    final Counter errorCounter;
+    public final Histogram histogram;
+    public final Counter totalCounter;
+    public final Counter errorCounter;
 
     CallCollectors(Histogram histogram, Counter totalCounter, Counter errorCounter) {
         this.histogram = histogram;
