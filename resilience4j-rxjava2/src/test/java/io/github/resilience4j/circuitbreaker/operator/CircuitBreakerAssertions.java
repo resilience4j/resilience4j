980ccf388ccb5ee37d5a6d96c5fb6d5ad95cd329
Daniel Imre <d4niel.imre@gmail.com>
2018-01-06 22:17:56 +0100

    Fix code style issue.

--- resilience4j-rxjava2/src/test/java/io/github/resilience4j/circuitbreaker/operator/CircuitBreakerAssertions.java
+++ resilience4j-rxjava2/src/test/java/io/github/resilience4j/circuitbreaker/operator/CircuitBreakerAssertions.java
@@ -8,21 +8,21 @@ import io.github.resilience4j.circuitbreaker.CircuitBreaker;
  * Helper class to test and assert circuit breakers.
  */
 class CircuitBreakerAssertions {
-    CircuitBreaker circuitBreaker = CircuitBreaker.ofDefaults("test");
+    protected final CircuitBreaker circuitBreaker = CircuitBreaker.ofDefaults("test");
 
-    void assertSingleSuccessfulCall() {
+    protected void assertSingleSuccessfulCall() {
         assertThat(circuitBreaker.getMetrics().getNumberOfBufferedCalls()).isEqualTo(1);
         assertThat(circuitBreaker.getMetrics().getNumberOfSuccessfulCalls()).isEqualTo(1);
         assertThat(circuitBreaker.getMetrics().getNumberOfFailedCalls()).isEqualTo(0);
     }
 
-    void assertSingleFailedCall() {
+    protected void assertSingleFailedCall() {
         assertThat(circuitBreaker.getMetrics().getNumberOfBufferedCalls()).isEqualTo(1);
         assertThat(circuitBreaker.getMetrics().getNumberOfSuccessfulCalls()).isEqualTo(0);
         assertThat(circuitBreaker.getMetrics().getNumberOfFailedCalls()).isEqualTo(1);
     }
 
-    void assertNoRegisteredCall() {
+    protected void assertNoRegisteredCall() {
         assertThat(circuitBreaker.getMetrics().getNumberOfBufferedCalls()).isEqualTo(0);
         assertThat(circuitBreaker.getMetrics().getNumberOfSuccessfulCalls()).isEqualTo(0);
         assertThat(circuitBreaker.getMetrics().getNumberOfFailedCalls()).isEqualTo(0);
