cc1a12ad9575ffb9a6ba58c98fc3de28dfa2201a
Mahmoud Romeh <mahmoud.romeh@collibra.com>
2019-04-04 15:44:38 +0200

    take async retry to separate class and reduce retry waiting time

--- resilience4j-spring-boot2/src/test/java/io/github/resilience4j/retry/RetryAutoConfigurationAsyncTest.java
+++ resilience4j-spring-boot2/src/test/java/io/github/resilience4j/retry/RetryAutoConfigurationAsyncTest.java
@@ -16,9 +16,15 @@
 package io.github.resilience4j.retry;
 
 import static io.github.resilience4j.service.test.retry.ReactiveRetryDummyService.BACKEND_C;
+import static io.github.resilience4j.service.test.retry.RetryDummyService.RETRY_BACKEND_A;
+import static io.github.resilience4j.service.test.retry.RetryDummyService.RETRY_BACKEND_B;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -30,18 +36,17 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import io.github.resilience4j.circuitbreaker.IgnoredException;
-import io.github.resilience4j.reactor.retry.RetryExceptionWrapper;
 import io.github.resilience4j.retry.autoconfigure.RetryProperties;
 import io.github.resilience4j.retry.configure.RetryAspect;
 import io.github.resilience4j.retry.monitoring.endpoint.RetryEndpointResponse;
 import io.github.resilience4j.retry.monitoring.endpoint.RetryEventsEndpointResponse;
 import io.github.resilience4j.service.test.TestApplication;
-import io.github.resilience4j.service.test.retry.ReactiveRetryDummyService;
+import io.github.resilience4j.service.test.retry.RetryDummyService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
 		classes = TestApplication.class)
-public class RetryAutoConfigurationReactorTest {
+public class RetryAutoConfigurationAsyncTest {
 
 	@Autowired
 	RetryRegistry retryRegistry;
@@ -53,60 +58,70 @@ public class RetryAutoConfigurationReactorTest {
 	RetryAspect retryAspect;
 
 	@Autowired
-	ReactiveRetryDummyService retryDummyService;
+	RetryDummyService retryDummyService;
 
 	@Autowired
 	private TestRestTemplate restTemplate;
 
 	/**
-	 * The test verifies that a Retry instance is created and configured properly when the RetryReactiveDummyService is invoked and
-	 * that the Retry logic is properly handled
+	 * The test verifies that a Async Retry instance is created and configured properly when the RetryDummyService is invoked and
+	 * that the Async Retry logic is properly handled
 	 */
 	@Test
 	@DirtiesContext
-	public void testRetryAutoConfigurationReactor() throws IOException {
+	public void testRetryAutoConfigurationAsync() throws Throwable {
 		assertThat(retryRegistry).isNotNull();
-		assertThat(retryProperties).isNotNull();
 
 		try {
-			retryDummyService.doSomethingFlux(true).subscribe(String::toUpperCase, throwable -> System.out.println("Exception received:" + throwable.getMessage()));
-		} catch (RetryExceptionWrapper ex) {
-			assertThat(ex.getCause()).hasCauseInstanceOf(IllegalArgumentException.class);
+			final CompletionStage<String> stringCompletionStage = retryDummyService.doSomethingAsync(true);
+			String result = awaitResult(stringCompletionStage, 5);
+			assertThat(result).isNull();
+
+		} catch (IOException ex) {
 			// Do nothing. The IOException is recorded by the retry as it is one of failure exceptions
+			assertThat(ex.getMessage()).contains("Test Message");
 		}
 		// The invocation is recorded by the CircuitBreaker as a success.
-		retryDummyService.doSomethingFlux(false).subscribe(String::toUpperCase, throwable -> System.out.println("Exception received:" + throwable.getMessage()));
-		;
-
-		Retry retry = retryRegistry.retry(BACKEND_C);
+		String resultSuccess = awaitResult(retryDummyService.doSomethingAsync(false), 5);
+		assertThat(resultSuccess).isNotEmpty();
+		Retry retry = retryRegistry.retry(RETRY_BACKEND_B);
 		assertThat(retry).isNotNull();
 
 		// expect retry is configured as defined in application.yml
 		assertThat(retry.getRetryConfig().getMaxAttempts()).isEqualTo(3);
-		assertThat(retry.getName()).isEqualTo(BACKEND_C);
+		assertThat(retry.getName()).isEqualTo(RETRY_BACKEND_B);
 		assertThat(retry.getRetryConfig().getExceptionPredicate().test(new IOException())).isTrue();
 
 		// expect retry actuator endpoint contains both retries
 		ResponseEntity<RetryEndpointResponse> retriesList = restTemplate.getForEntity("/actuator/retries", RetryEndpointResponse.class);
-		assertThat(retriesList.getBody().getRetries()).hasSize(3);
+		assertThat(retriesList.getBody().getRetries()).hasSize(3).containsOnly(RETRY_BACKEND_A, RETRY_BACKEND_B, BACKEND_C);
 
 		// expect retry-event actuator endpoint recorded both events
 		ResponseEntity<RetryEventsEndpointResponse> retryEventList = restTemplate.getForEntity("/actuator/retryevents", RetryEventsEndpointResponse.class);
 		assertThat(retryEventList.getBody().getRetryEvents()).hasSize(3);
 
-		retryEventList = restTemplate.getForEntity("/actuator/retryevents/" + BACKEND_C, RetryEventsEndpointResponse.class);
+		retryEventList = restTemplate.getForEntity("/actuator/retryevents/" + RETRY_BACKEND_B, RetryEventsEndpointResponse.class);
 		assertThat(retryEventList.getBody().getRetryEvents()).hasSize(3);
 
-		assertThat(retry.getRetryConfig().getExceptionPredicate().test(new IllegalArgumentException())).isTrue();
+		assertThat(retry.getRetryConfig().getExceptionPredicate().test(new IOException())).isTrue();
 		assertThat(retry.getRetryConfig().getExceptionPredicate().test(new IgnoredException())).isFalse();
-
-		// expect aspect configured as defined in application.yml
-		assertThat(retryAspect.getOrder()).isEqualTo(399);
 
 		assertThat(retry.getMetrics().getNumberOfFailedCallsWithoutRetryAttempt()).isEqualTo(0);
 		assertThat(retry.getMetrics().getNumberOfFailedCallsWithRetryAttempt()).isEqualTo(1);
 		assertThat(retry.getMetrics().getNumberOfSuccessfulCallsWithoutRetryAttempt()).isEqualTo(1);
 		assertThat(retry.getMetrics().getNumberOfSuccessfulCallsWithRetryAttempt()).isEqualTo(0);
+		// expect aspect configured as defined in application.yml
+		assertThat(retryAspect.getOrder()).isEqualTo(399);
 	}
 
+
+	private <T> T awaitResult(CompletionStage<T> completionStage, long timeoutSeconds) throws Throwable {
+		try {
+			return completionStage.toCompletableFuture().get(timeoutSeconds, TimeUnit.SECONDS);
+		} catch (InterruptedException | TimeoutException e) {
+			throw new AssertionError(e);
+		} catch (ExecutionException e) {
+			throw e.getCause();
+		}
+	}
 }
