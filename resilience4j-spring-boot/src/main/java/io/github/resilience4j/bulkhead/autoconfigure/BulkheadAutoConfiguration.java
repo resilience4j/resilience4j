f2fa2678e466ceda329e61e01107f7c95d99df73
Mahmoud Romeh <mahmoud.romeh@collibra.com>
2019-04-05 14:38:34 +0200

    fix event endpoint bean lack of configuration

--- resilience4j-spring-boot/src/main/java/io/github/resilience4j/bulkhead/autoconfigure/BulkheadAutoConfiguration.java
+++ resilience4j-spring-boot/src/main/java/io/github/resilience4j/bulkhead/autoconfigure/BulkheadAutoConfiguration.java
@@ -15,10 +15,6 @@
  */
 package io.github.resilience4j.bulkhead.autoconfigure;
 
-import io.github.resilience4j.bulkhead.Bulkhead;
-import io.github.resilience4j.bulkhead.BulkheadRegistry;
-import io.github.resilience4j.bulkhead.configure.BulkheadConfiguration;
-import io.github.resilience4j.bulkhead.monitoring.endpoint.BulkheadEndpoint;
 import org.springframework.boot.actuate.autoconfigure.EndpointAutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
@@ -26,6 +22,14 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+
+import io.github.resilience4j.bulkhead.Bulkhead;
+import io.github.resilience4j.bulkhead.BulkheadRegistry;
+import io.github.resilience4j.bulkhead.configure.BulkheadConfiguration;
+import io.github.resilience4j.bulkhead.event.BulkheadEvent;
+import io.github.resilience4j.bulkhead.monitoring.endpoint.BulkheadEndpoint;
+import io.github.resilience4j.bulkhead.monitoring.endpoint.BulkheadEventsEndpoint;
+import io.github.resilience4j.consumer.EventConsumerRegistry;
 
 /**
  * {@link org.springframework.boot.autoconfigure.EnableAutoConfiguration
@@ -42,4 +46,9 @@ public class BulkheadAutoConfiguration {
     public BulkheadEndpoint bulkheadEndpoint(BulkheadRegistry bulkheadRegistry) {
         return new BulkheadEndpoint(bulkheadRegistry);
     }
+
+	@Bean
+	public BulkheadEventsEndpoint bulkheadEventsEndpoint(EventConsumerRegistry<BulkheadEvent> eventConsumerRegistry, BulkheadRegistry bulkheadRegistry) {
+		return new BulkheadEventsEndpoint(eventConsumerRegistry, bulkheadRegistry);
+	}
 }
