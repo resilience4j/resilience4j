6b18cf10f9500f8d313ba3e6c751eefb9bae7275
robwin <robwin@t-online.de>
2019-05-02 11:08:57 +0200

    Added support for Micrometer to Spring Boot 1 starter.

--- resilience4j-spring-boot/src/main/java/io/github/resilience4j/circuitbreaker/autoconfigure/CircuitBreakerMicrometerAutoConfiguration.java
+++ resilience4j-spring-boot/src/main/java/io/github/resilience4j/circuitbreaker/autoconfigure/CircuitBreakerMicrometerAutoConfiguration.java
@@ -18,7 +18,7 @@ package io.github.resilience4j.circuitbreaker.autoconfigure;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
 import io.github.resilience4j.micrometer.CircuitBreakerMetrics;
 import io.github.resilience4j.micrometer.tagged.TaggedCircuitBreakerMetrics;
-import io.micrometer.spring.autoconfigure.CompositeMeterRegistryAutoConfiguration;
+import io.micrometer.spring.autoconfigure.MetricsAutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -31,8 +31,8 @@ import org.springframework.context.annotation.Configuration;
  * Auto-configuration} for resilience4j-metrics.
  */
 @Configuration
-@ConditionalOnClass(CompositeMeterRegistryAutoConfiguration.class)
-@AutoConfigureAfter(value = {CircuitBreakerAutoConfiguration.class, CompositeMeterRegistryAutoConfiguration.class})
+@ConditionalOnClass(MetricsAutoConfiguration.class)
+@AutoConfigureAfter(value = {CircuitBreakerAutoConfiguration.class, MetricsAutoConfiguration.class})
 @ConditionalOnProperty(value = "resilience4j.circuitbreaker.metrics.enabled", matchIfMissing = true)
 public class CircuitBreakerMicrometerAutoConfiguration {
 
