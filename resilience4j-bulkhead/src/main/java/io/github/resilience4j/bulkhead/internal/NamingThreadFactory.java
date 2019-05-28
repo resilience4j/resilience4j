b2ed9540021572ced10f81d09b72489e1e977302
Tomasz Skowroński <5780288+hexmind@users.noreply.github.com>
2019-05-28 08:30:48 +0200

    Use bulkhead name for threads naming (#473)

---
+++ resilience4j-bulkhead/src/main/java/io/github/resilience4j/bulkhead/internal/NamingThreadFactory.java
@@ -0,0 +1,41 @@
+package io.github.resilience4j.bulkhead.internal;
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Creates threads using "bulkhead-$name-%d" pattern for naming.
+ * Is based on {@link java.util.concurrent.Executors.DefaultThreadFactory}.
+ */
+class NamingThreadFactory implements ThreadFactory {
+
+    private final ThreadGroup group;
+    private final AtomicInteger threadNumber = new AtomicInteger(1);
+    private final String prefix;
+
+    NamingThreadFactory(String name) {
+        this.group = getThreadGroup();
+        this.prefix = String.join("-", "bulkhead", name, "");
+    }
+
+    private ThreadGroup getThreadGroup() {
+        SecurityManager security = System.getSecurityManager();
+        return security != null ? security.getThreadGroup() : Thread.currentThread().getThreadGroup();
+    }
+
+    @Override
+    public Thread newThread(Runnable runnable) {
+        Thread thread = new Thread(group, runnable, createName(), 0);
+        if (thread.isDaemon()) {
+            thread.setDaemon(false);
+        }
+        if (thread.getPriority() != Thread.NORM_PRIORITY) {
+            thread.setPriority(Thread.NORM_PRIORITY);
+        }
+        return thread;
+    }
+
+    private String createName() {
+        return prefix + threadNumber.getAndIncrement();
+    }
+}
