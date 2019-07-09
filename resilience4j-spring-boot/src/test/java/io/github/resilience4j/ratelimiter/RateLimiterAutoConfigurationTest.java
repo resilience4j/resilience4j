bf2d962141c5e292b2dfae1af0d348eadb45ca63
MRomeh <mahmoud.romeh@gmail.com>
2019-07-09 15:28:45 +0200

    Issue #515: Aspects ordering update (#534)
    * #515  ordering update
    
    * #515 aspects ordering update
    
    * #515 unit test update
    
    * #515 test coverage increase

--- resilience4j-spring-boot/src/test/java/io/github/resilience4j/ratelimiter/RateLimiterAutoConfigurationTest.java
+++ resilience4j-spring-boot/src/test/java/io/github/resilience4j/ratelimiter/RateLimiterAutoConfigurationTest.java
@@ -30,7 +30,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.core.Ordered;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -129,6 +128,6 @@ public class RateLimiterAutoConfigurationTest {
             .until(() -> rateLimiter.getMetrics().getAvailablePermissions() == 10);
 
 
-	    assertThat(rateLimiterAspect.getOrder()).isEqualTo(Ordered.LOWEST_PRECEDENCE - 1);
+        assertThat(rateLimiterAspect.getOrder()).isEqualTo(401);
     }
 }
