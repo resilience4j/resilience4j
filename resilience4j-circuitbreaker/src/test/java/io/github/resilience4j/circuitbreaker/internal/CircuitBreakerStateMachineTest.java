c5771c12cc2272b075c510ae73dcc7663eaf2c25
Robert Winkler <robwin@t-online.de>
2019-08-16 08:04:46 +0200

    Issue #547: Added new metrics to Spring Boot HealthIndicator. (#569)

--- resilience4j-circuitbreaker/src/test/java/io/github/resilience4j/circuitbreaker/internal/CircuitBreakerStateMachineTest.java
+++ resilience4j-circuitbreaker/src/test/java/io/github/resilience4j/circuitbreaker/internal/CircuitBreakerStateMachineTest.java
@@ -422,6 +422,56 @@ public class CircuitBreakerStateMachineTest {
     }
 
     @Test
+    public void shouldIgnoreExceptionsAndThenTransitionToClosed() {
+        circuitBreaker.transitionToOpenState();
+        circuitBreaker.transitionToHalfOpenState();
+        assertThat(circuitBreaker.getState()).isEqualTo(CircuitBreaker.State.HALF_OPEN);
+
+        assertThat(circuitBreaker.tryAcquirePermission()).isEqualTo(true);
+        // Should ignore NumberFormatException and release permission
+        circuitBreaker.onError(0, TimeUnit.NANOSECONDS, new NumberFormatException());
+        assertThat(circuitBreaker.getState()).isEqualTo(CircuitBreaker.State.HALF_OPEN);
+
+        assertThat(circuitBreaker.tryAcquirePermission()).isEqualTo(true);
+        // Should ignore NumberFormatException and release permission
+        circuitBreaker.onError(0, TimeUnit.NANOSECONDS, new NumberFormatException());
+        assertThat(circuitBreaker.getState()).isEqualTo(CircuitBreaker.State.HALF_OPEN);
+
+        assertThat(circuitBreaker.tryAcquirePermission()).isEqualTo(true);
+        // Should ignore NumberFormatException and release permission
+        circuitBreaker.onError(0, TimeUnit.NANOSECONDS, new NumberFormatException());
+        assertThat(circuitBreaker.getState()).isEqualTo(CircuitBreaker.State.HALF_OPEN);
+
+        assertThat(circuitBreaker.tryAcquirePermission()).isEqualTo(true);
+        // Should ignore NumberFormatException and release permission
+        circuitBreaker.onError(0, TimeUnit.NANOSECONDS, new NumberFormatException());
+        assertThat(circuitBreaker.getState()).isEqualTo(CircuitBreaker.State.HALF_OPEN);
+
+        assertCircuitBreakerMetricsEqualTo(-1f, 0, 0, 0, 0L);
+
+        assertThat(circuitBreaker.tryAcquirePermission()).isEqualTo(true);
+        circuitBreaker.onSuccess(0, TimeUnit.NANOSECONDS); //
+        assertThat(circuitBreaker.getState()).isEqualTo(CircuitBreaker.State.HALF_OPEN);
+
+        assertThat(circuitBreaker.tryAcquirePermission()).isEqualTo(true);
+        circuitBreaker.onSuccess(0, TimeUnit.NANOSECONDS); //
+        assertThat(circuitBreaker.getState()).isEqualTo(CircuitBreaker.State.HALF_OPEN);
+
+        assertThat(circuitBreaker.tryAcquirePermission()).isEqualTo(true);
+        circuitBreaker.onSuccess(0, TimeUnit.NANOSECONDS); //
+        assertThat(circuitBreaker.getState()).isEqualTo(CircuitBreaker.State.HALF_OPEN);
+
+        assertCircuitBreakerMetricsEqualTo(-1f, 3, 3, 0, 0L);
+
+        assertThat(circuitBreaker.tryAcquirePermission()).isEqualTo(true);
+        circuitBreaker.onSuccess(0, TimeUnit.NANOSECONDS); //
+        assertThat(circuitBreaker.getState()).isEqualTo(CircuitBreaker.State.CLOSED);
+
+        assertCircuitBreakerMetricsEqualTo(-1f, 0, 0, 0, 0L);
+    }
+
+
+    @Test
     public void shouldNotAllowTransitionFromClosedToHalfOpen() {
         assertThatThrownBy(() -> circuitBreaker.transitionToHalfOpenState()).isInstanceOf(IllegalStateTransitionException.class)
                 .hasMessage("CircuitBreaker 'testName' tried an illegal state transition from CLOSED to HALF_OPEN");
--- resilience4j-spring-boot-common/src/main/java/io/github/resilience4j/circuitbreaker/monitoring/health/CircuitBreakerHealthIndicator.java
+++ resilience4j-spring-boot-common/src/main/java/io/github/resilience4j/circuitbreaker/monitoring/health/CircuitBreakerHealthIndicator.java
@@ -29,9 +29,12 @@ import java.util.Optional;
 public class CircuitBreakerHealthIndicator implements HealthIndicator {
 
     private static final String FAILURE_RATE = "failureRate";
-    private static final String FAILURE_RATE_THRESHOLD = "setFailureRateThreshold";
+    private static final String SLOW_CALL_RATE = "slowCallRate";
+    private static final String FAILURE_RATE_THRESHOLD = "failureRateThreshold";
+    private static final String SLOW_CALL_RATE_THRESHOLD = "slowCallRateThreshold";
     private static final String BUFFERED_CALLS = "bufferedCalls";
     private static final String FAILED_CALLS = "failedCalls";
+    private static final String SLOW_CALLS = "slowCalls";
     private static final String NOT_PERMITTED = "notPermittedCalls";
     private static final String STATE = "state";
     private final CircuitBreaker circuitBreaker;
@@ -65,7 +68,10 @@ public class CircuitBreakerHealthIndicator implements HealthIndicator {
         CircuitBreakerConfig config = circuitBreaker.getCircuitBreakerConfig();
         builder.withDetail(FAILURE_RATE, metrics.getFailureRate() + "%")
             .withDetail(FAILURE_RATE_THRESHOLD, config.getFailureRateThreshold() + "%")
+            .withDetail(SLOW_CALL_RATE, metrics.getSlowCallRate() + "%")
+            .withDetail(SLOW_CALL_RATE_THRESHOLD, config.getSlowCallRateThreshold() + "%")
             .withDetail(BUFFERED_CALLS, metrics.getNumberOfBufferedCalls())
+            .withDetail(SLOW_CALLS, metrics.getNumberOfSlowCalls())
             .withDetail(FAILED_CALLS, metrics.getNumberOfFailedCalls())
             .withDetail(NOT_PERMITTED, metrics.getNumberOfNotPermittedCalls())
             .withDetail(STATE, circuitBreaker.getState());
