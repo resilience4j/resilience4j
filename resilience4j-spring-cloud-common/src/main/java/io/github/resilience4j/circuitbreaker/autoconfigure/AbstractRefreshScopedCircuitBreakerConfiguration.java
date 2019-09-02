53f460d8d84cc669e3e63b63a8789b01d0b9228c
Ingyu Hwang <dlsrb6342@gmail.com>
2019-09-02 16:40:52 +0900

    Issue #546: Added resilience4j-spring-cloud modules (#550)

---
+++ resilience4j-spring-cloud-common/src/main/java/io/github/resilience4j/circuitbreaker/autoconfigure/AbstractRefreshScopedCircuitBreakerConfiguration.java
@@ -0,0 +1,47 @@
+package io.github.resilience4j.circuitbreaker.autoconfigure;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import io.github.resilience4j.circuitbreaker.configure.CircuitBreakerConfiguration;
+import io.github.resilience4j.circuitbreaker.configure.CircuitBreakerConfigurationProperties;
+import io.github.resilience4j.circuitbreaker.event.CircuitBreakerEvent;
+import io.github.resilience4j.consumer.EventConsumerRegistry;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public abstract class AbstractRefreshScopedCircuitBreakerConfiguration {
+
+    protected final ConfigurableBeanFactory beanFactory;
+    protected final CircuitBreakerConfiguration circuitBreakerConfiguration;
+    protected final CircuitBreakerConfigurationProperties circuitBreakerProperties;
+
+    protected AbstractRefreshScopedCircuitBreakerConfiguration(ConfigurableBeanFactory beanFactory,
+                                                        CircuitBreakerConfigurationProperties circuitBreakerProperties) {
+        this.beanFactory = beanFactory;
+        this.circuitBreakerProperties = circuitBreakerProperties;
+        this.circuitBreakerConfiguration = new CircuitBreakerConfiguration(circuitBreakerProperties);
+    }
+
+    /**
+     * @param eventConsumerRegistry the circuit breaker event consumer registry
+     * @return the RefreshScoped CircuitBreakerRegistry
+     */
+    @Bean
+    @RefreshScope
+    @ConditionalOnMissingBean
+    public CircuitBreakerRegistry circuitBreakerRegistry(EventConsumerRegistry<CircuitBreakerEvent> eventConsumerRegistry) {
+        CircuitBreakerRegistry circuitBreakerRegistry = circuitBreakerConfiguration.createCircuitBreakerRegistry(circuitBreakerProperties);
+
+        // Register the event consumers
+        circuitBreakerConfiguration.registerEventConsumer(circuitBreakerRegistry, eventConsumerRegistry);
+
+        // Initialize backends that were initially configured.
+        circuitBreakerConfiguration.initCircuitBreakerRegistry(circuitBreakerRegistry);
+
+        return circuitBreakerRegistry;
+    }
+
+}
---
+++ resilience4j-spring-cloud2/src/main/java/io/github/resilience4j/circuitbreaker/autoconfigure/RefreshScopedCircuitBreakerAutoConfiguration.java
@@ -0,0 +1,24 @@
+package io.github.resilience4j.circuitbreaker.autoconfigure;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.configure.CircuitBreakerConfigurationProperties;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
+import org.springframework.cloud.context.scope.refresh.RefreshScope;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnClass({CircuitBreaker.class, RefreshScope.class})
+@AutoConfigureAfter(RefreshAutoConfiguration.class)
+@AutoConfigureBefore(CircuitBreakerAutoConfiguration.class)
+public class RefreshScopedCircuitBreakerAutoConfiguration extends AbstractRefreshScopedCircuitBreakerConfiguration {
+
+    public RefreshScopedCircuitBreakerAutoConfiguration(ConfigurableBeanFactory beanFactory,
+                                                        CircuitBreakerConfigurationProperties circuitBreakerProperties) {
+        super(beanFactory, circuitBreakerProperties);
+    }
+
+}
