eef77ee6dfd4b21ee1e7194b74615705052f2f13
Hexmind Tomasz Skowroński <5780288+hexmind@users.noreply.github.com>
2019-09-16 19:09:38 +0200

    TimeLimiter as rx transformer (#235)

---
+++ resilience4j-rxjava2/src/test/java/io/github/resilience4j/timelimiter/transformer/TimeLimiterTransformerSingleTest.java
@@ -0,0 +1,96 @@
+/*
+ *  Copyright 2019 authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.resilience4j.timelimiter.transformer;
+
+import io.github.resilience4j.TestSchedulerRule;
+import io.github.resilience4j.timelimiter.TimeLimiter;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import io.reactivex.Single;
+import io.reactivex.observers.TestObserver;
+import io.reactivex.schedulers.TestScheduler;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.mock;
+
+public class TimeLimiterTransformerSingleTest {
+
+    @Rule
+    public final TestSchedulerRule testSchedulerRule = new TestSchedulerRule();
+    private final TestScheduler testScheduler = testSchedulerRule.getTestScheduler();
+    private final TimeLimiter timeLimiter = mock(TimeLimiter.class);
+
+    @Test
+    public void otherError() {
+        given(timeLimiter.getTimeLimiterConfig())
+                .willReturn(toConfig(Duration.ZERO));
+        TestObserver<?> observer = Single.error(new RuntimeException())
+                .compose(TimeLimiterTransformer.of(timeLimiter))
+                .test();
+
+        testScheduler.advanceTimeBy(1, TimeUnit.MINUTES);
+
+        observer.assertError(RuntimeException.class);
+        then(timeLimiter).should()
+                .onError(any(RuntimeException.class));
+    }
+
+    @Test
+    public void timeout() {
+        given(timeLimiter.getTimeLimiterConfig())
+                .willReturn(toConfig(Duration.ZERO));
+        TestObserver<?> observer = Single.timer(1, TimeUnit.MINUTES)
+                .compose(TimeLimiterTransformer.of(timeLimiter))
+                .test();
+
+        testScheduler.advanceTimeBy(1, TimeUnit.MINUTES);
+
+        observer.assertError(TimeoutException.class);
+        then(timeLimiter).should()
+                .onError(any(TimeoutException.class));
+    }
+
+    @Test
+    public void doNotTimeout() {
+        given(timeLimiter.getTimeLimiterConfig())
+                .willReturn(toConfig(Duration.ofMinutes(1)));
+        TestObserver<?> observer = Single.timer(1, TimeUnit.SECONDS)
+                .compose(TimeLimiterTransformer.of(timeLimiter))
+                .test();
+
+        testScheduler.advanceTimeBy(1, TimeUnit.MINUTES);
+
+        observer.assertValueCount(1);
+        then(timeLimiter).should()
+                .onSuccess();
+    }
+
+    private TimeLimiterConfig toConfig(Duration timeout) {
+        return TimeLimiterConfig.custom()
+                .timeoutDuration(timeout)
+                .build();
+    }
+
+}
