a1c1bcc723c814f2ed2be46ba675f7ff95423ab1
Marco Ferrer <35935108+marcoferrer@users.noreply.github.com>
2019-10-08 18:36:18 -0400

    update flow time limiter impl

--- resilience4j-kotlin/src/main/kotlin/io/github/resilience4j/kotlin/timelimiter/FlowTimeLimiter.kt
+++ resilience4j-kotlin/src/main/kotlin/io/github/resilience4j/kotlin/timelimiter/FlowTimeLimiter.kt
@@ -21,16 +21,18 @@ package io.github.resilience4j.kotlin.timelimiter
 import io.github.resilience4j.timelimiter.TimeLimiter
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.emitAll
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.channelFlow
+import kotlinx.coroutines.flow.collect
 
 
 @UseExperimental(ExperimentalCoroutinesApi::class)
 fun <T> Flow<T>.timeLimiter(timeLimiter: TimeLimiter): Flow<T> {
     val source = this
-    return flow {
+    return channelFlow {
         timeLimiter.executeSuspendFunction {
-            emitAll(source)
+            source.collect {
+                send(it)
+            }
         }
     }
 }
