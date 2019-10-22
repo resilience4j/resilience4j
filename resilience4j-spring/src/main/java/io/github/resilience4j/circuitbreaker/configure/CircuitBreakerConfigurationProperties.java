0676f8b75866088a050d1ba84c0e1ceff78b3ce8
MRomeh <mahmoud.romeh@gmail.com>
2019-10-22 08:39:34 +0200

    Issue #699: Added wait interval spring boot support (#708)

--- resilience4j-spring/src/main/java/io/github/resilience4j/circuitbreaker/configure/CircuitBreakerConfigurationProperties.java
+++ resilience4j-spring/src/main/java/io/github/resilience4j/circuitbreaker/configure/CircuitBreakerConfigurationProperties.java
@@ -15,8 +15,6 @@
  */
 package io.github.resilience4j.circuitbreaker.configure;
 
-
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 
 public class CircuitBreakerConfigurationProperties extends io.github.resilience4j.common.circuitbreaker.configuration.CircuitBreakerConfigurationProperties {
