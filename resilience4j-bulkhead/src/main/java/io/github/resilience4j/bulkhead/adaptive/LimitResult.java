d43737513504fcb79bfff45f5bf4824134590d6c
Mahmoud Romeh <mahmoud.romeh@collibra.com>
2019-10-31 11:06:15 +0100

    #review comments

--- resilience4j-bulkhead/src/main/java/io/github/resilience4j/bulkhead/adaptive/LimitResult.java
+++ resilience4j-bulkhead/src/main/java/io/github/resilience4j/bulkhead/adaptive/LimitResult.java
@@ -1,9 +1,11 @@
 package io.github.resilience4j.bulkhead.adaptive;
 
+import io.github.resilience4j.core.metrics.Snapshot;
+
 import java.util.Objects;
 
 /**
- * limit result DTO
+ * the adapted new limit result that will be calculated through the configured {@link LimitPolicy#adaptLimitIfAny(Snapshot, int)}
  */
 public class LimitResult {
 
@@ -15,10 +17,16 @@ public class LimitResult {
 		this.waitTime = waitTime;
 	}
 
+	/**
+	 * @return the the adapted bulkhead limit based into the configured {@link LimitPolicy} calculation
+	 */
 	public int getLimit() {
 		return limit;
 	}
 
+	/**
+	 * @return the the adapted bulkhead wait time based into the configured {@link LimitPolicy} calculation
+	 */
 	public long waitTime() {
 		return waitTime;
 	}
