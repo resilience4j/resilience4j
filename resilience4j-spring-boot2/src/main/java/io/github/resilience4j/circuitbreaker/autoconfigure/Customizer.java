76ecebacc713b48eb9e8eb0cfb5fff325c3cf511
Ingyu Hwang <dlsrb6342@gmail.com>
2019-12-19 16:20:46 +0900

    Delete Customizer.java (#781)

--- resilience4j-spring-boot2/src/main/java/io/github/resilience4j/circuitbreaker/autoconfigure/Customizer.java
+++
@@ -1,9 +0,0 @@
-package io.github.resilience4j.circuitbreaker.autoconfigure;
-
-import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
-import io.vavr.Tuple2;
-
-public interface Customizer<T> {
-
-    Tuple2<String, CircuitBreakerConfig.Builder> customize(CircuitBreakerConfig.Builder builder);
-}
