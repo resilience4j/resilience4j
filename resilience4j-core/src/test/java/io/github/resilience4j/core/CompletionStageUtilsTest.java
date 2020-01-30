97d941db68e9aa0e9fa130d66281e8b1cd05382a
Robert Winkler <rwinkler@telekom.de>
2020-01-30 14:46:05 +0100

    Added a recover method to CompletionStageUtils which allow to recover from a specific exception type.

--- resilience4j-core/src/test/java/io/github/resilience4j/core/CompletionStageUtilsTest.java
+++ resilience4j-core/src/test/java/io/github/resilience4j/core/CompletionStageUtilsTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.function.Function;
 
 import static io.github.resilience4j.core.CompletionStageUtils.recover;
@@ -23,9 +24,9 @@ public class CompletionStageUtilsTest {
     }
 
     @Test
-    public void shouldRecoverException() throws Exception {
+    public void shouldRecoverTimeoutException() throws Exception {
         CompletableFuture<String> future = new CompletableFuture<>();
-        future.completeExceptionally(new RuntimeException());
+        future.completeExceptionally(new TimeoutException());
 
         String result = recover(future, (e) -> "fallback").toCompletableFuture()
             .get(1, TimeUnit.SECONDS);
@@ -46,6 +47,39 @@ public class CompletionStageUtilsTest {
 
         assertThatThrownBy(() -> recover(future, fallback).toCompletableFuture()
             .get(1, TimeUnit.SECONDS)).hasCause(exception);
+    }
+
+    @Test
+    public void shouldReturnResult2() throws Exception {
+        CompletableFuture<String> future = CompletableFuture.completedFuture("result");
+
+        String result = recover(future, TimeoutException.class, (e) -> "fallback").toCompletableFuture()
+            .get(1, TimeUnit.SECONDS);
+
+        assertThat(result).isEqualTo("result");
+    }
+
+    @Test
+    public void shouldThrowRuntimeException() {
+        RuntimeException exception = new RuntimeException("blub");
+
+        CompletableFuture<String> future = new CompletableFuture<>();
+        future.completeExceptionally(exception);
+
+        assertThatThrownBy(() -> recover(future, TimeoutException.class, (e) -> "fallback").toCompletableFuture()
+            .get(1, TimeUnit.SECONDS))
+            .hasCause(exception);
+    }
+
+    @Test
+    public void shouldRecoverTimeoutException2() throws Exception {
+        CompletableFuture<String> future = new CompletableFuture<>();
+        future.completeExceptionally(new TimeoutException());
+
+        String result = recover(future, (e) -> "fallback").toCompletableFuture()
+            .get(1, TimeUnit.SECONDS);
+
+        assertThat(result).isEqualTo("fallback");
     }
 
 }
