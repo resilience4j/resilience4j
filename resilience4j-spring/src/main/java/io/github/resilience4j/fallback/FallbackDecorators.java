c01d51388b34674c5d5c529da06c29a3084b1606
Robert Winkler <rwinkler@telekom.de>
2020-02-03 13:43:07 +0100

    Fixed FallbackConfiguration and FallbackConfiguratioOnMissingBean configuration class. The bean declarations required more specific return types.

--- resilience4j-spring/src/main/java/io/github/resilience4j/fallback/FallbackDecorators.java
+++ resilience4j-spring/src/main/java/io/github/resilience4j/fallback/FallbackDecorators.java
@@ -50,4 +50,8 @@ public class FallbackDecorators {
             .findFirst()
             .orElse(defaultFallbackDecorator);
     }
+
+    public List<FallbackDecorator> getFallbackDecorators() {
+        return fallbackDecorators;
+    }
 }
