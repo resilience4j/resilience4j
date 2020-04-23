6ffd1725554296e037b3e9ffe65538d7d1620bf7
Michael <polli104@mail.chapman.edu>
2020-04-23 14:41:45 -0700

    added intercepter for ratelimiter

---
+++ resilience4j-micronaut/src/main/java/io/github/resilience4j/ratelimiter/RateLimiterAnnotationMapper.java
@@ -0,0 +1,24 @@
+package io.github.resilience4j.ratelimiter;
+
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.micronaut.core.annotation.AnnotationValue;
+import io.micronaut.inject.annotation.NamedAnnotationMapper;
+import io.micronaut.inject.visitor.VisitorContext;
+
+import javax.annotation.Nonnull;
+import java.lang.annotation.Annotation;
+import java.util.Collections;
+import java.util.List;
+
+public class RateLimiterAnnotationMapper implements NamedAnnotationMapper {
+    @Nonnull
+    @Override
+    public String getName() {
+        return "io.github.resilience4j.annotation.RateLimiter";
+    }
+
+    @Override
+    public List<AnnotationValue<?>> map(AnnotationValue<Annotation> annotation, VisitorContext visitorContext) {
+        return Collections.singletonList(AnnotationValue.builder(RateLimiter.class).build());
+    }
+}
---
+++ resilience4j-micronaut/src/main/java/io/github/resilience4j/ratelimiter/RateLimiterSpecificationInterceptor.java
@@ -0,0 +1,71 @@
+package io.github.resilience4j.ratelimiter;
+
+
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.ratelimiter.operator.RateLimiterOperator;
+import io.micronaut.aop.Interceptor;
+import io.micronaut.aop.InvocationContext;
+import io.micronaut.aop.MethodInterceptor;
+import io.micronaut.aop.MethodInvocationContext;
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.core.async.publisher.Publishers;
+import io.micronaut.core.convert.ConversionService;
+import io.micronaut.core.type.ReturnType;
+import io.micronaut.inject.ExecutableMethod;
+import io.reactivex.Flowable;
+
+import javax.inject.Singleton;
+import java.util.Map;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Singleton
+public class RateLimiterSpecificationInterceptor implements MethodInterceptor<Object, Object> {
+
+    private final RateLimiterRegistry rateLimiterRegistry;
+
+    public RateLimiterSpecificationInterceptor(RateLimiterRegistry rateLimiterRegistry) {
+        this.rateLimiterRegistry = rateLimiterRegistry;
+    }
+
+    private final Map<ExecutableMethod, TransactionInvocation> transactionInvocationMap = new ConcurrentHashMap<>(30);
+
+
+    @Override
+    public Object intercept(MethodInvocationContext<Object, Object> context) {
+        ExecutableMethod executableMethod = context.getExecutableMethod();
+        final String name = executableMethod.stringValue(RateLimiter.class).orElse("default");
+        io.github.resilience4j.ratelimiter.RateLimiter rateLimiter = this.rateLimiterRegistry.rateLimiter(name);
+
+        ReturnType<Object> rt = context.getReturnType();
+        Class<Object> returnType = rt.getType();
+        if (CompletionStage.class.isAssignableFrom(returnType)) {
+            Object result = context.proceed();
+            if (result == null) {
+                return result;
+            } else {
+                return null;
+            }
+
+
+        } else if (Publishers.isConvertibleToPublisher(returnType)) {
+            ConversionService<?> conversionService = ConversionService.SHARED;
+            Object result = context.proceed();
+            if (result == null) {
+                return result;
+            } else {
+                Flowable<?> observable = conversionService
+                    .convert(result, Flowable.class)
+                    .orElseThrow(() -> new IllegalStateException("Unconvertible Reactive type: " + result));
+                return observable.compose(RateLimiterOperator.of(rateLimiter));
+            }
+        }
+        return null;
+    }
+    /**
+     * Cached invocation associating a method with a definition a transaction manager.
+     */
+    private final class TransactionInvocation {
+
+    }
+}
