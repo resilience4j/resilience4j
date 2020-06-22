25626d273a60e35f9e41ac9747467c0f5d239467
Mahmoud Romeh <mahmoud.romeh@collibra.com>
2020-06-22 17:07:15 +0200

    #issue-866 add spring actuator rest endpoints for force open , disable and close specific circuit breaker instance

--- resilience4j-spring-boot2/src/main/java/io/github/resilience4j/circuitbreaker/monitoring/endpoint/CircuitBreakerEndpoint.java
+++ resilience4j-spring-boot2/src/main/java/io/github/resilience4j/circuitbreaker/monitoring/endpoint/CircuitBreakerEndpoint.java
@@ -19,9 +19,14 @@ package io.github.resilience4j.circuitbreaker.monitoring.endpoint;
 import io.github.resilience4j.circuitbreaker.CircuitBreaker;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
 import io.github.resilience4j.common.circuitbreaker.monitoring.endpoint.CircuitBreakerEndpointResponse;
+import io.github.resilience4j.common.circuitbreaker.monitoring.endpoint.CircuitBreakerUpdateStateResponse;
+import io.github.resilience4j.common.circuitbreaker.monitoring.endpoint.UpdateState;
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.annotation.Selector;
+import org.springframework.boot.actuate.endpoint.annotation.WriteOperation;
 
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -39,5 +44,34 @@ public class CircuitBreakerEndpoint {
         List<String> circuitBreakers = circuitBreakerRegistry.getAllCircuitBreakers()
             .map(CircuitBreaker::getName).sorted().toJavaList();
         return new CircuitBreakerEndpointResponse(circuitBreakers);
+    }
+
+    @WriteOperation
+    public CircuitBreakerUpdateStateResponse updateCircuitBreakerState(@Selector String name, UpdateState updateState) {
+        final CircuitBreaker circuitBreaker = circuitBreakerRegistry.circuitBreaker(name);
+        final String message = "%s state has been changed successfully";
+        switch (updateState) {
+            case CLOSE:
+                circuitBreaker.transitionToClosedState();
+                return createCircuitBreakerUpdateStateResponse(name, circuitBreaker.getState().toString(), String.format(message, name));
+            case FORCE_OPEN:
+                circuitBreaker.transitionToForcedOpenState();
+                return createCircuitBreakerUpdateStateResponse(name, circuitBreaker.getState().toString(), String.format(message, name));
+            case DISABLE:
+                circuitBreaker.transitionToDisabledState();
+                return createCircuitBreakerUpdateStateResponse(name, circuitBreaker.getState().toString(), String.format(message, name));
+            default:
+                return createCircuitBreakerUpdateStateResponse(name, circuitBreaker.getState().toString(), "State change value is not supported please use only " + Arrays.toString(UpdateState.values()));
+        }
+
+    }
+
+    private CircuitBreakerUpdateStateResponse createCircuitBreakerUpdateStateResponse(String circuitBreakerName, String newState, String message) {
+        CircuitBreakerUpdateStateResponse circuitBreakerUpdateStateResponse = new CircuitBreakerUpdateStateResponse();
+        circuitBreakerUpdateStateResponse.setCircuitBreakerName(circuitBreakerName);
+        circuitBreakerUpdateStateResponse.setCurrentState(newState);
+        circuitBreakerUpdateStateResponse.setMessage(message);
+
+        return circuitBreakerUpdateStateResponse;
     }
 }
