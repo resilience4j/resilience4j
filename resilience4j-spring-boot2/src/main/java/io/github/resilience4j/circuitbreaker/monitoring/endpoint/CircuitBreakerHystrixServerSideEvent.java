e1c3b712a90427f08d08efc8ff0bcf3a32a77a54
z002fdh <vijayalayan.ramalingam@target.com>
2020-08-02 14:14:32 -0500

    Adding copywrite to all the new classes and refactoring

--- resilience4j-spring-boot2/src/main/java/io/github/resilience4j/circuitbreaker/monitoring/endpoint/CircuitBreakerHystrixServerSideEvent.java
+++ resilience4j-spring-boot2/src/main/java/io/github/resilience4j/circuitbreaker/monitoring/endpoint/CircuitBreakerHystrixServerSideEvent.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020 Vijay Ram
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.github.resilience4j.circuitbreaker.monitoring.endpoint;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -17,11 +32,6 @@ import reactor.core.publisher.Flux;
 import java.util.function.BiFunction;
 
 import static io.github.resilience4j.reactor.adapter.ReactorAdapter.toFlux;
-
-/**
- * @author vijayram
- */
-
 /**
  * This class is used to produce Circuit breaker events as streams in hystrix like fashion
  * <p>
@@ -37,7 +47,6 @@ import static io.github.resilience4j.reactor.adapter.ReactorAdapter.toFlux;
  * This is created as a bridge to support the legacy hystrix eco system of monitoring tools especially for
  * those that are migrating from hystrix to resilence4j to continue to use hystrix eco tools.
  */
-
 @Endpoint(id = "hystrix-stream-circuitbreaker-events")
 public class CircuitBreakerHystrixServerSideEvent {
 
--- resilience4j-spring-boot2/src/main/java/io/github/resilience4j/circuitbreaker/monitoring/endpoint/CircuitBreakerServerSideEvent.java
+++ resilience4j-spring-boot2/src/main/java/io/github/resilience4j/circuitbreaker/monitoring/endpoint/CircuitBreakerServerSideEvent.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020 Vijay Ram
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.github.resilience4j.circuitbreaker.monitoring.endpoint;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -17,10 +32,6 @@ import reactor.core.publisher.Flux;
 import java.util.function.Function;
 
 import static io.github.resilience4j.reactor.adapter.ReactorAdapter.toFlux;
-
-/**
- * @author vijayram
- */
 
 /**
  * This class is used to produce Circuit breaker events as streams.
