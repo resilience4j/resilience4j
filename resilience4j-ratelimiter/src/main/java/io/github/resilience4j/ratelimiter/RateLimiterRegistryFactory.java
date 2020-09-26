c21441c78b33a466ae88270c2a72a64f48c859ad
gkatzioura <gkatzioura@gmail.com>
2020-09-26 13:06:34 +0100

    Issue #1138: Added comment on RateLimiterRegistryFactory

--- resilience4j-ratelimiter/src/main/java/io/github/resilience4j/ratelimiter/RateLimiterRegistryFactory.java
+++ resilience4j-ratelimiter/src/main/java/io/github/resilience4j/ratelimiter/RateLimiterRegistryFactory.java
@@ -1,3 +1,21 @@
+/*
+ *
+ *  Copyright 2020 Emmanouil Gkatziouras
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ */
 package io.github.resilience4j.ratelimiter;
 
 import io.github.resilience4j.core.RegistryStore;
@@ -7,6 +25,11 @@ import io.github.resilience4j.ratelimiter.internal.InMemoryRateLimiterRegistry;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Due to the {@link RateLimiterRegistry} creating RateLimiters when non existing
+ * a factory method will create the equivalent RateLimiter.
+ * @param <C>
+ */
 interface RateLimiterRegistryFactory<C extends RateLimiterConfig> {
 
     RateLimiterRegistry create(Map<String, C> configs,
@@ -14,11 +37,11 @@ interface RateLimiterRegistryFactory<C extends RateLimiterConfig> {
                                io.vavr.collection.Map<String, String> tags, RegistryStore<RateLimiter> registryStore);
 
     static RateLimiterRegistryFactory<RateLimiterConfig> defaultConfig() {
-        return (configs, registryEventConsumers, tags, registryStore) -> InMemoryRateLimiterRegistry.create(configs,registryEventConsumers,tags, registryStore);
+        return InMemoryRateLimiterRegistry::create;
     }
 
     static RateLimiterRegistryFactory<RefillRateLimiterConfig> refillConfig() {
-        return (configs, registryEventConsumers, tags, registryStore) -> InMemoryRateLimiterRegistry.createRefill(configs,registryEventConsumers,tags, registryStore);
+        return InMemoryRateLimiterRegistry::createRefill;
     }
 
 }
