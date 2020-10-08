51498b4046b0eab4d2a0f251463ccfa272adc939
Mahmoud Romeh <mahmoud.romeh@collibra.com>
2020-10-08 21:40:00 +0200

    rolling back wrong commit

--- resilience4j-spring-boot2/src/test/java/io/github/resilience4j/service/test/bulkhead/BulkheadReactiveDummyService.java
+++ resilience4j-spring-boot2/src/test/java/io/github/resilience4j/service/test/bulkhead/BulkheadReactiveDummyService.java
@@ -1,9 +1,7 @@
 package io.github.resilience4j.service.test.bulkhead;
 
-import io.reactivex.*;
+import io.reactivex.Flowable;
 import reactor.core.publisher.Flux;
-
-import java.io.IOException;
 
 public interface BulkheadReactiveDummyService {
 
@@ -12,12 +10,4 @@ public interface BulkheadReactiveDummyService {
     Flux<String> doSomethingFlux();
 
     Flowable<String> doSomethingFlowable();
-
-    Maybe<String> doSomethingMaybe(boolean throwException) throws IOException;
-
-    Single<String> doSomethingSingle(boolean throwException) throws IOException;
-
-    Completable doSomethingCompletable(boolean throwException) throws IOException;
-
-    Observable<String> doSomethingObservable(boolean throwException) throws IOException;
 }
--- resilience4j-spring-boot2/src/test/java/io/github/resilience4j/service/test/bulkhead/BulkheadReactiveDummyServiceImpl.java
+++ resilience4j-spring-boot2/src/test/java/io/github/resilience4j/service/test/bulkhead/BulkheadReactiveDummyServiceImpl.java
@@ -1,13 +1,12 @@
 package io.github.resilience4j.service.test.bulkhead;
 
 import io.github.resilience4j.bulkhead.annotation.Bulkhead;
-import io.reactivex.*;
+import io.reactivex.Flowable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 
-import java.io.IOException;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
@@ -26,25 +25,5 @@ public class BulkheadReactiveDummyServiceImpl implements BulkheadReactiveDummySe
     @Override
     public Flowable<String> doSomethingFlowable() {
         return Flowable.just("testMaybe").delay(500, TimeUnit.MILLISECONDS);
-    }
-
-    @Override
-    public Maybe<String> doSomethingMaybe(boolean throwException) throws IOException {
-        return null;
-    }
-
-    @Override
-    public Single<String> doSomethingSingle(boolean throwException) throws IOException {
-        return null;
-    }
-
-    @Override
-    public Completable doSomethingCompletable(boolean throwException) throws IOException {
-        return null;
-    }
-
-    @Override
-    public Observable<String> doSomethingObservable(boolean throwException) throws IOException {
-        return null;
     }
 }
