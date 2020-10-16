50c9a18596e0a58505b4c1844da25ebe402dc5e4
gkatzioura <gkatzioura@gmail.com>
2020-10-16 21:19:25 +0100

    Issue #1138: fixed doc

--- resilience4j-ratelimiter/src/main/java/io/github/resilience4j/ratelimiter/internal/BaseAtomicLimiter.java
+++ resilience4j-ratelimiter/src/main/java/io/github/resilience4j/ratelimiter/internal/BaseAtomicLimiter.java
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright 2020 Emmanouil Gkatziouras
+ *  Copyright 2016 Robert Winkler, Bohdan Storozhuk and Emmanouil Gkatziouras
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
