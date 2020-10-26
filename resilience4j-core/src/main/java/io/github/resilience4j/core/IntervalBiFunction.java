7952a615b232cbf6e78c4dbbaedc29157c471475
Cosmin Seceleanu <coosmin26@yahoo.com>
2020-10-26 10:51:26 +0200

    Issue #1043: Added IntervalBiFunction to RetryConfig to calculate wait duration based on result/exception (#1200)

---
+++ resilience4j-core/src/main/java/io/github/resilience4j/core/IntervalBiFunction.java
@@ -0,0 +1,18 @@
+package io.github.resilience4j.core;
+
+import io.vavr.control.Either;
+
+import java.util.function.BiFunction;
+
+/**
+ * An IntervalBiFunction which can be used to calculate the wait interval. The input parameters of the bi
+ * function is the number of attempts (attempt) and either result or exception, the output parameter is the wait interval in
+ * milliseconds. The attempt parameter starts at 1 and increases with every further attempt.
+ */
+@FunctionalInterface
+public interface IntervalBiFunction<T> extends BiFunction<Integer, Either<Throwable, T>, Long> {
+
+    static <T> IntervalBiFunction<T> ofIntervalFunction(IntervalFunction f) {
+        return (attempt, either) -> f.apply(attempt);
+    }
+}
