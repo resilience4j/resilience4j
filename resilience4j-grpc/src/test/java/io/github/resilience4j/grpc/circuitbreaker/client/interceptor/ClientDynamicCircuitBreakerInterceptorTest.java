c8ff73e09e94d8c01ba0a6b58c504c308d368085
Ken Dombeck <kdombeck@gmail.com>
2021-01-14 21:01:36 -0600

    Use latest version of gRPC

--- resilience4j-grpc/src/test/java/io/github/resilience4j/grpc/circuitbreaker/client/interceptor/ClientDynamicCircuitBreakerInterceptorTest.java
+++ resilience4j-grpc/src/test/java/io/github/resilience4j/grpc/circuitbreaker/client/interceptor/ClientDynamicCircuitBreakerInterceptorTest.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2019 Marco Ferrer
+ *  Copyright 2019 Marco Ferrer, Ken Dombeck
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
