0120ad5e84c0037284d32c0067f704a45c862815
Robert Winkler <rwinkler@telekom.de>
2021-02-02 08:30:46 +0100

    Issue #1322: Fixed copyright headers and author annotations.

--- resilience4j-core/src/main/java/io/github/resilience4j/core/lang/NonNull.java
+++ resilience4j-core/src/main/java/io/github/resilience4j/core/lang/NonNull.java
@@ -1,20 +1,17 @@
 /*
+ * Copyright 2002-2019 the original author or authors.
  *
- *  Copyright 2017: Robert Winkler
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
- *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package io.github.resilience4j.core.lang;
 
@@ -23,17 +20,21 @@ import javax.annotation.meta.TypeQualifierNickname;
 import java.lang.annotation.*;
 
 /**
- * A common annotation to declare that annotated elements cannot be {@code null}. Leverages JSR 305
- * meta-annotations to indicate nullability in Java to common tools with JSR 305 support and used by
- * Kotlin to infer nullability of the API.
- * <p>
+ * A common Spring annotation to declare that annotated elements cannot be {@code null}.
+ *
+ * <p>Leverages JSR-305 meta-annotations to indicate nullability in Java to common
+ * tools with JSR-305 support and used by Kotlin to infer nullability of Spring API.
+ *
  * <p>Should be used at parameter, return value, and field level. Method overrides should
  * repeat parent {@code @NonNull} annotations unless they behave differently.
- * <p>
- * <p>Use {@code @NonNullApi} (scope = parameters + return values) and/or {@code @NonNullFields}
- * (scope = fields) to set the default behavior to non-nullable in order to avoid annotating your
- * whole codebase with {@code @NonNull}.
  *
+ * <p>Use {@code @NonNullApi} (scope = parameters + return values) and/or {@code @NonNullFields}
+ * (scope = fields) to set the default behavior to non-nullable in order to avoid annotating
+ * your whole codebase with {@code @NonNull}.
+ *
+ * @author Sebastien Deleuze
+ * @author Juergen Hoeller
+ * @since 5.0
  * @see NonNullApi
  * @see NonNullFields
  * @see Nullable
@@ -44,5 +45,4 @@ import java.lang.annotation.*;
 @Nonnull
 @TypeQualifierNickname
 public @interface NonNull {
-
 }
