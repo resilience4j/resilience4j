eb9dee4e2ec1da61e8c0496db9a71a60990ddadb
Ipuvi <ipuvi.mishra@miqdigital.com>
2021-04-03 21:20:10 +0530

    Updated javadocs

--- resilience4j-retrofit/src/main/java/io/github/resilience4j/retrofit/RetrofitRetry.java
+++ resilience4j-retrofit/src/main/java/io/github/resilience4j/retrofit/RetrofitRetry.java
@@ -1,3 +1,18 @@
+/*
+ *
+ * Copyright 2021 Ipuvi Mishra
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ */
 package io.github.resilience4j.retrofit;
 
 import io.github.resilience4j.retrofit.internal.DecoratedCall;
@@ -18,7 +33,8 @@ import retrofit2.Response;
  */
 public interface RetrofitRetry {
 
-    /**Zoom
+    /**
+     * Zoom
      * Decorate {@link Call}s allow Retry functionality.
      *
      * @param retry Retry to apply
