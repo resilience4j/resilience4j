1a4a71641b064ea1b7eacfb303d865ef6ec5c064
Kaze <hejunhuiok@hotmail.com>
2021-05-25 14:45:03 +0800

    Issue #1437: MetricsOnlyState should publish each event at most once (#1438)

--- resilience4j-circuitbreaker/src/main/java/io/github/resilience4j/circuitbreaker/internal/CircuitBreakerStateMachine.java
+++ resilience4j-circuitbreaker/src/main/java/io/github/resilience4j/circuitbreaker/internal/CircuitBreakerStateMachine.java
@@ -877,11 +877,11 @@ public final class CircuitBreakerStateMachine implements CircuitBreaker {
             }
 
             if (shouldPublishFailureRateExceededEvent(result)) {
-                publishCircuitThresholdsExceededEvent(result, circuitBreakerMetrics);
+                publishCircuitFailureRateExceededEvent(getName(), circuitBreakerMetrics.getFailureRate());
             }
 
             if (shouldPublishSlowCallRateExceededEvent(result)) {
-                publishCircuitThresholdsExceededEvent(result, circuitBreakerMetrics);
+                publishCircuitSlowCallRateExceededEvent(getName(), circuitBreakerMetrics.getSlowCallRate());
             }
         }
 
