12f66a31d7b4619ed0ffd1fddee9da683c41b9b7
seokgoon28 <148044191+seokgoon28@users.noreply.github.com>
2023-11-18 17:23:29 +0900

    Support class name using SpEL expression at Circuitbreaker annotation name field. (#2053)

--- resilience4j-spring/src/main/java/io/github/resilience4j/spelresolver/SpelRootObject.java
+++ resilience4j-spring/src/main/java/io/github/resilience4j/spelresolver/SpelRootObject.java
@@ -21,12 +21,18 @@ import java.lang.reflect.Method;
  * SpEL #root class
  */
 public class SpelRootObject {
+    private final String className;
     private final String methodName;
     private final Object[] args;
 
     public SpelRootObject(Method method, Object[] args) {
+        this.className = method.getDeclaringClass().getName();
         this.methodName = method.getName();
         this.args = args;
+    }
+
+    public String getClassName() {
+        return className;
     }
 
     public String getMethodName() {
