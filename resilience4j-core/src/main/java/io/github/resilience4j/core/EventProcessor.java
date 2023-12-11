bc0ba4239d942c8c768d01962a5e65f762cd5a8b
shijun_deng <dengshijun1992@qq.com>
2023-12-11 23:29:10 +0800

    avoid to add the same event listener repeatedly
    avoid to add the same event listener repeatedly

--- resilience4j-core/src/main/java/io/github/resilience4j/core/EventProcessor.java
+++ resilience4j-core/src/main/java/io/github/resilience4j/core/EventProcessor.java
@@ -20,14 +20,14 @@ package io.github.resilience4j.core;
 
 import io.github.resilience4j.core.lang.Nullable;
 
-import java.util.List;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 public class EventProcessor<T> implements EventPublisher<T> {
 
-    final List<EventConsumer<T>> onEventConsumers = new CopyOnWriteArrayList<>();
+    final Set<EventConsumer<T>> onEventConsumers = new CopyOnWriteArraySet<>();
     final ConcurrentMap<String, List<EventConsumer<T>>> eventConsumerMap = new ConcurrentHashMap<>();
     private boolean consumerRegistered;
 
@@ -39,7 +39,7 @@ public class EventProcessor<T> implements EventPublisher<T> {
     public synchronized void registerConsumer(String className, EventConsumer<? extends T> eventConsumer) {
         this.eventConsumerMap.compute(className, (k, consumers) -> {
             if (consumers == null) {
-                consumers = new CopyOnWriteArrayList<>();
+                consumers = new CopyOnWriteArraySet<>();
                 consumers.add((EventConsumer<T>) eventConsumer);
                 return consumers;
             } else {
@@ -60,7 +60,7 @@ public class EventProcessor<T> implements EventPublisher<T> {
         }
 
         if (!eventConsumerMap.isEmpty()) {
-            final List<EventConsumer<T>> consumers = this.eventConsumerMap.get(event.getClass().getName());
+            final Set<EventConsumer<T>> consumers = this.eventConsumerMap.get(event.getClass().getName());
             if (consumers != null && !consumers.isEmpty()) {
                 for (EventConsumer<T> consumer : consumers) {
                     consumer.consumeEvent(event);
