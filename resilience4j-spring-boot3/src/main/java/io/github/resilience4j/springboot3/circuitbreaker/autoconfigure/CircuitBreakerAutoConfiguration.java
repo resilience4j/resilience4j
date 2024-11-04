81efbf5de354a7266a1d6e0c7b44458f87c418cb
Olivier-Pimpare-Charbonneau <ocharb@mac.com>
2024-11-04 15:29:00 -0800

    Implement custom ConverterRegistry to register the IgnoreUnknownExceptionConverter.

--- resilience4j-spring-boot3/src/main/java/io/github/resilience4j/springboot3/circuitbreaker/autoconfigure/CircuitBreakerAutoConfiguration.java
+++ resilience4j-spring-boot3/src/main/java/io/github/resilience4j/springboot3/circuitbreaker/autoconfigure/CircuitBreakerAutoConfiguration.java
@@ -17,6 +17,7 @@ package io.github.resilience4j.springboot3.circuitbreaker.autoconfigure;
 
 import io.github.resilience4j.circuitbreaker.CircuitBreaker;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import io.github.resilience4j.circuitbreaker.ConverterRegistry;
 import io.github.resilience4j.circuitbreaker.event.CircuitBreakerEvent;
 import io.github.resilience4j.circuitbreaker.internal.IgnoreUnknownExceptionConverter;
 import io.github.resilience4j.springboot3.circuitbreaker.monitoring.endpoint.CircuitBreakerEndpoint;
@@ -31,10 +32,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.core.convert.converter.Converter;
-import org.springframework.core.convert.converter.ConverterFactory;
-import org.springframework.core.convert.converter.ConverterRegistry;
-import org.springframework.core.convert.converter.GenericConverter;
 import org.springframework.core.env.AbstractEnvironment;
 import org.springframework.core.env.EnumerablePropertySource;
 
@@ -75,38 +72,9 @@ public class CircuitBreakerAutoConfiguration {
 
     @Bean
     public ConverterRegistry converterRegistry(IgnoreUnknownExceptionConverter ignoreUnknownExceptionConverter) {
-        ConverterRegistry registry = getConverterRegistry();
-        registry.addConverter(ignoreUnknownExceptionConverter);
+        ConverterRegistry registry = new ConverterRegistry();
+        registry.registerConverter(String.class, ignoreUnknownExceptionConverter);
         return registry;
-    }
-
-    private ConverterRegistry getConverterRegistry() {
-        return new ConverterRegistry() {
-            @Override
-            public void addConverter(Converter<?, ?> converter) {
-
-            }
-
-            @Override
-            public <S, T> void addConverter(Class<S> sourceType, Class<T> targetType, Converter<? super S, ? extends T> converter) {
-
-            }
-
-            @Override
-            public void addConverter(GenericConverter converter) {
-
-            }
-
-            @Override
-            public void addConverterFactory(ConverterFactory<?, ?> factory) {
-
-            }
-
-            @Override
-            public void removeConvertible(Class<?> sourceType, Class<?> targetType) {
-
-            }
-        };
     }
 
     @Configuration
