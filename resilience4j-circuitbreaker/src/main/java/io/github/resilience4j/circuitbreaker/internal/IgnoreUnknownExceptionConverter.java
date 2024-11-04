43c2183bddc20e2e498e081f8e58626fc325468f
Olivier-Pimpare-Charbonneau <ocharb@mac.com>
2024-11-04 15:24:54 -0800

    Implement custom converter.

--- resilience4j-circuitbreaker/src/main/java/io/github/resilience4j/circuitbreaker/internal/IgnoreUnknownExceptionConverter.java
+++ resilience4j-circuitbreaker/src/main/java/io/github/resilience4j/circuitbreaker/internal/IgnoreUnknownExceptionConverter.java
@@ -1,6 +1,6 @@
 package io.github.resilience4j.circuitbreaker.internal;
 
-import org.springframework.core.convert.converter.Converter;
+import io.github.resilience4j.core.Converter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
