57278344ad2e8f048fb4ab80d3ca41ec58d96ee9
Olivier-Pimpare-Charbonneau <ocharb@mac.com>
2024-12-01 16:03:54 -0800

    Update tests to reflect changes.

--- resilience4j-spring/src/test/java/io/github/resilience4j/bulkhead/configure/PlainObjectBulkheadAspectExtTest.java
+++ resilience4j-spring/src/test/java/io/github/resilience4j/bulkhead/configure/PlainObjectBulkheadAspectExtTest.java
@@ -1,6 +1,5 @@
 package io.github.resilience4j.bulkhead.configure;
 
-import io.github.resilience4j.bulkhead.internal.ThreadPoolBulkheadAdapter;
 import org.junit.*;
 import org.mockito.*;
 import reactor.core.publisher.*;
@@ -21,7 +20,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 @RunWith(MockitoJUnitRunner.class)
 public class PlainObjectBulkheadAspectExtTest {
 
-    private final String TEST_METHOD = "testMethod";
+    private final String METHOD_NAME = "testMethod";
     private final String BULKHEAD_NAME = "test";
 
     @Mock
@@ -46,48 +45,13 @@ public class PlainObjectBulkheadAspectExtTest {
     }
 
     @Test
-    public void testBulkheadTypeSemaphoreReturnsPlainObject() throws Throwable{
-        String expected = "Plain Result";
-
-        when(proceedingJoinPoint.proceed()).thenReturn(expected);
-
-        Object actual = plainObjectBulkHeadAspectExt.handle(proceedingJoinPoint, bulkhead, TEST_METHOD);
-
-        assertThat(actual).isEqualTo(expected);
-    }
-
-    @Test
     public void testBulkheadTypeThreadPoolReturnsPlainObject() throws Throwable {
         String expected = "ThreadPool Result";
         when(proceedingJoinPoint.proceed()).thenReturn(expected);
 
-        ThreadPoolBulkheadConfig config = ThreadPoolBulkheadConfig.custom()
-                .maxThreadPoolSize(5)
-                .coreThreadPoolSize(2)
-                .queueCapacity(10)
-                .build();
-
-        ThreadPoolBulkhead threadPoolBulkhead = ThreadPoolBulkhead.of(BULKHEAD_NAME, config);
-        bulkhead = new ThreadPoolBulkheadAdapter(threadPoolBulkhead);
-        Object actual = plainObjectBulkHeadAspectExt.handle(proceedingJoinPoint, bulkhead, TEST_METHOD);
+        Object actual = plainObjectBulkHeadAspectExt.handle(proceedingJoinPoint, bulkhead, METHOD_NAME);
 
         assertThat(actual).isEqualTo(expected);
-    }
-
-    @Test
-    public void testThrowableIsThrownAndCaught() throws Throwable {
-        String expectedMessage = "Test Exception";
-        Throwable throwable = new Throwable(expectedMessage);
-        when(proceedingJoinPoint.proceed()).thenThrow(throwable);
-
-        try {
-            plainObjectBulkHeadAspectExt.handle(proceedingJoinPoint, bulkhead, TEST_METHOD);
-        } catch (RuntimeException ex) {
-            assertThat(ex.getCause()).isInstanceOf(Throwable.class);
-            assertThat(ex.getCause().getMessage()).isEqualTo(ex.getCause().getMessage());
-        }
-
-        verify(timeLimiterRegistry).timeLimiter(BULKHEAD_NAME);
     }
 
     @Test
@@ -98,27 +62,9 @@ public class PlainObjectBulkheadAspectExtTest {
         });
 
         try {
-            plainObjectBulkHeadAspectExt.handle(proceedingJoinPoint, bulkhead, TEST_METHOD);
+            plainObjectBulkHeadAspectExt.handle(proceedingJoinPoint, bulkhead, METHOD_NAME);
         } catch (TimeoutException ex) {
             assertThat(ex.getMessage()).contains("did not complete within time");
-        }
-
-        verify(timeLimiterRegistry).timeLimiter(BULKHEAD_NAME);
-    }
-
-    @Test
-    public void testBulkheadFullExceptionIsThrownAndCaught() throws Throwable {
-        String expectedMessage = "Bulkhead 'test' is full and does not permit further calls";
-        BulkheadFullException bulkheadFullException = BulkheadFullException.createBulkheadFullException(bulkhead);
-        when(proceedingJoinPoint.proceed()).thenThrow(bulkheadFullException);
-
-        try {
-            plainObjectBulkHeadAspectExt.handle(proceedingJoinPoint, bulkhead, TEST_METHOD);
-        } catch (RuntimeException ex) {
-            assertThat(ex.getCause()).isInstanceOf(ExecutionException.class);
-            ExecutionException executionException = (ExecutionException) ex.getCause();
-            assertThat(executionException.getCause()).isInstanceOf(BulkheadFullException.class);
-            assertThat(executionException.getCause().getMessage()).isEqualTo(expectedMessage);
         }
 
         verify(timeLimiterRegistry).timeLimiter(BULKHEAD_NAME);
