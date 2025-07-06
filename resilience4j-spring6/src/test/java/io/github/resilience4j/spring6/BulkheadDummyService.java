2dd2c5cf2e9ff0c13fc0df11669edd0c573ce547
Dominique Villard <dominique.villard@doctolib.com>
2025-07-06 18:23:24 +0200

    Add bulkhead tests.

--- resilience4j-spring6/src/test/java/io/github/resilience4j/spring6/BulkheadDummyService.java
+++ resilience4j-spring6/src/test/java/io/github/resilience4j/spring6/BulkheadDummyService.java
@@ -119,4 +119,16 @@ public class BulkheadDummyService implements TestDummyService {
     public String spelSync(String backend) {
         return syncError();
     }
+
+    @Override
+    @Bulkhead(name = "#root.args[0]", fallbackMethod = "#{'recovery'}")
+    public String spelSyncNoCfg(String backend) {
+        return backend;
+    }
+
+    @Override
+    @Bulkhead(name = "#root.args[0]", configuration = BACKEND, fallbackMethod = "#{'recovery'}")
+    public String spelSyncWithCfg(String backend) {
+        return backend;
+    }
 }
--- resilience4j-spring6/src/test/java/io/github/resilience4j/spring6/bulkhead/configure/BulkHeadInitializationInAspectTest.java
+++ resilience4j-spring6/src/test/java/io/github/resilience4j/spring6/bulkhead/configure/BulkHeadInitializationInAspectTest.java
@@ -1,8 +1,10 @@
 package io.github.resilience4j.spring6.bulkhead.configure;
 
+import io.github.resilience4j.bulkhead.Bulkhead;
 import io.github.resilience4j.bulkhead.BulkheadConfig;
 import io.github.resilience4j.bulkhead.BulkheadRegistry;
 import io.github.resilience4j.spring6.TestDummyService;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -53,10 +55,40 @@ public class BulkHeadInitializationInAspectTest {
         assertThat(registry.getAllBulkheads()).isEmpty();
     }
 
+    @After
+    public void tearDown() {
+        // could have used DirtiesContext but spawns a springboot for each test
+        registry.getAllBulkheads().stream().map(Bulkhead::getName).forEach(registry::remove);
+    }
+
     @Test
     public void testCorrectConfigIsUsedInAspect() {
-
         // The bulkhead is configured to allow 0 concurrent calls, so the call should be rejected
         assertThat(testDummyService.syncSuccess()).isEqualTo("recovered");
     }
+
+    @Test
+    public void testSpelWithoutMappingConfigurationInAspect() {
+        // Default bulkhead is configured to allow several concurrent calls
+        assertThat(testDummyService.spelSyncNoCfg("foo")).isEqualTo("foo");
+        assertThat(registry.getAllBulkheads()).hasSize(1).first()
+                .matches(bulkhead -> bulkhead.getName().equals("foo"))
+                .matches(bulkhead -> bulkhead.getBulkheadConfig() == registry.getDefaultConfig());
+    }
+
+    @Test
+    public void testSpelWithMappingConfigurationInAspect() {
+        // The bulkhead is configured to allow 0 concurrent calls, so the call should be rejected
+        assertThat(testDummyService.spelSyncWithCfg("foo")).isEqualTo("recovered");
+        assertThat(registry.getAllBulkheads()).hasSize(1).first()
+                .matches(bulkhead -> bulkhead.getName().equals("foo"))
+                .matches(bulkhead -> bulkhead.getBulkheadConfig() != registry.getDefaultConfig());
+    }
+
+    @Test
+    public void testConfigurationKeyIsUsedInAspect() {
+        assertThat(testDummyService.spelSyncWithCfg(BACKEND)).isEqualTo("recovered");
+        assertThat(testDummyService.spelSyncNoCfg(BACKEND)).isEqualTo("recovered");
+    }
+
 }
