377260f86631e3ee8c1fc9258b1ecd343608ee66
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2026-03-06 01:59:32 +0900

    Issue #2189: Remove manual flaky reproduction tests from PR scope

--- resilience4j-spring-boot3/src/test/java/io/github/resilience4j/springboot3/service/test/CbOnlyReproService.java
+++
@@ -1,8 +0,0 @@
-package io.github.resilience4j.springboot3.service.test;
-
-import java.io.IOException;
-
-public interface CbOnlyReproService {
-
-    void doSomething(boolean throwBackendTrouble) throws IOException;
-}
