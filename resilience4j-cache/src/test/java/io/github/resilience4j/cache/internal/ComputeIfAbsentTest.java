412aa6a3c1f5ec9bf6ffd696abd30bf9b357b658
Artur Havliukovskyi <a.havliukovskyi@pm.me>
2026-03-29 11:04:20 +0200

    migrate resilience4j-cache tests from JUnit 4 to JUnit Jupiter

--- resilience4j-cache/src/test/java/io/github/resilience4j/cache/internal/ComputeIfAbsentTest.java
+++ resilience4j-cache/src/test/java/io/github/resilience4j/cache/internal/ComputeIfAbsentTest.java
@@ -1,30 +1,49 @@
+/*
+ *
+ * Copyright 2026
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ *
+ */
 package io.github.resilience4j.cache.internal;
 
 import io.github.resilience4j.cache.internal.CacheImpl.ComputeIfAbsent;
 import io.github.resilience4j.core.functions.CheckedSupplier;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import javax.cache.processor.MutableEntry;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 
-public class ComputeIfAbsentTest {
+class ComputeIfAbsentTest {
 
     private MutableEntry<String, String> mockEntry;
 
     @SuppressWarnings("unchecked")
-    @Before
-    public void setUp() {
+    @BeforeEach
+    void setUp() {
         mockEntry = mock(MutableEntry.class);
     }
 
     @SuppressWarnings("unchecked")
     @Test
-    public void shouldReturnCachedValueIfCached() {
+    void shouldReturnCachedValueIfCached() {
         given(mockEntry.exists()).willReturn(true);
         given(mockEntry.getValue()).willReturn("Cached Value");
         CheckedSupplier<String> mockedSupplier = mock(CheckedSupplier.class);
@@ -36,7 +55,7 @@ public class ComputeIfAbsentTest {
     }
 
     @Test
-    public void shouldReturnSuppliedValueIfNotCached() throws Throwable {
+    void shouldReturnSuppliedValueIfNotCached() throws Throwable {
         given(mockEntry.exists()).willReturn(false);
         CheckedSupplier<String> supplier = spy(new SpyableSupplier("Supplied Value"));
 
@@ -48,7 +67,7 @@ public class ComputeIfAbsentTest {
     }
 
     @Test
-    public void shouldRethrowSupplierException() {
+    void shouldRethrowSupplierException() {
         given(mockEntry.exists()).willReturn(false);
         CheckedSupplier<String> supplier = () -> {
             throw new RuntimeException("Boom!");
