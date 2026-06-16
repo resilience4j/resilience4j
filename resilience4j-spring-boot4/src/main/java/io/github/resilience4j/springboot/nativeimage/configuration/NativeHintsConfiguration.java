c2b60d15aee43b8def1f1a3628b3114a3020c214
noy-solvin <noy@solvin.ai>
2026-06-16 13:09:15 +0300

    Update NativeHintsConfiguration.java

--- resilience4j-spring-boot4/src/main/java/io/github/resilience4j/springboot/nativeimage/configuration/NativeHintsConfiguration.java
+++ resilience4j-spring-boot4/src/main/java/io/github/resilience4j/springboot/nativeimage/configuration/NativeHintsConfiguration.java
@@ -28,8 +28,8 @@ public class NativeHintsConfiguration implements RuntimeHintsRegistrar {
         hints.reflection().registerType(io.github.resilience4j.spring6.fallback.FallbackMethod.class,
             builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_METHODS, MemberCategory.INVOKE_DECLARED_CONSTRUCTORS));
         hints.reflection().registerType(io.github.resilience4j.spring6.utils.AnnotationExtractor.class,
-            builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_METHODS);
+            builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_METHODS));
         hints.reflection().registerType(org.springframework.context.expression.MethodBasedEvaluationContext.class,
-            builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_METHODS);
+            builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_METHODS));
     }
 }
